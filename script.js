621fc944721f1dfe109d50e148a668672a5b486e
anascsk <anascsk@gmail.com>
2021-08-05 22:16:40 +0530

    js

--- script.js
+++ script.js
@@ -51,3 +51,16 @@ function showMovies(movies) {
     }
 }
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
